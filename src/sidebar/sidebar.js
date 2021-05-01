03bed34ce93ab58838091dda1abf7c8685477dd6
Syedmha <syedabedi12@gmail.com>
2021-05-01 23:36:05 +0530

    Update sidebar.js

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -19,7 +19,7 @@ const Sidebar = ({ classes, note, setNote }) => {
 
   useEffect(() => {
     setNote({ ...note, selectedNote: allNotes[note.selectedNoteIndex] });
-  }, [allNotes, selectedNote, setNote, note,]);
+  }, [allNotes, note.selectedNote, setNote, note,]);
 
   const createNewNote = () => {
     setNewNote({ ...newNote, addingNote: !addingNote });
